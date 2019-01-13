b8136f16df657fec66c0458aa119ce18eac5f34b
Evan <3959074+binary-signal@user.noreply.com>
2019-01-13 02:35:34 +0200

    Working on converting data returned from the api into dataframes

--- main.py
+++ main.py
@@ -52,18 +52,19 @@ if __name__ == "__main__":
 
     try:
         # fetch data
-        data = entsoe.transmission_grid_unavailability(from_date, to_date,
-                                                       country=country,
-                                                       outage_type=outage_type,
-                                                       asset_type=asset_type,
-                                                       outage_status=outage_status)
+        data = entsoe. \
+            transmission_grid_unavailability(from_date, to_date,
+                                             country=country,
+                                             outage_type=outage_type,
+                                             asset_type=asset_type,
+                                             outage_status=outage_status)
 
         pickle.dump(data, open("data.pckl", "wb"))
-        data_df = entsoe.data_table_to_df(data)
 
         # fetch details for data
         ids = [d['detailId'] for d in data]
-        details = entsoe_client.API.details_grid_unavailability_batch(entsoe, ids)
+        details = entsoe_client.API.details_grid_unavailability_batch(entsoe,
+                                                                      ids)
         pickle.dump(details, open("data_detail.pckl", "wb"))
 
         """
@@ -71,7 +72,7 @@ if __name__ == "__main__":
         
         Fetching time series data can be very time consuming, due to big amount
         of data and number of api calls resulting in rate limiting or remote
-        host disconnections. In order to improve speed and avoid getting black - 
+        host disconnections. In order to improve speed and avoid getting black  
         listed from the server use 'days_to_fetch' or 'skip_past_data' params
         or both, this way data slicing is implemented on api calls similarly to 
         slicing a python list object, but this time you are slicing the data
@@ -80,9 +81,9 @@ if __name__ == "__main__":
             skip_past_data: fetch time series data from today's date until the 
                             end of interval, skips downloading past days data
             
-            days_to_fetch: specify how many days worth of data to fetch, a day is
-                            60 datapoints. e.g. days_to_fetch=2 fetches  120 data
-                            points and ignores the rest time series data.
+            days_to_fetch: specify how many days worth of data to fetch, a day 
+                        is 60 data points. e.g. days_to_fetch=2 fetches  120 
+                        data points and ignores the rest time series data.
                             
         """
         ids_interval = [[row['detailId'], row['dateStart'], row['dateEnd']]
@@ -91,6 +92,7 @@ if __name__ == "__main__":
             curve_grid_unavailability_batch(entsoe, ids_interval,
                                             days_to_fetch=2,
                                             skip_past_data=False)
+
         pickle.dump(timeseries, open("data_series.pckl", "wb"))
 
         logging.info("session completed successfully")
